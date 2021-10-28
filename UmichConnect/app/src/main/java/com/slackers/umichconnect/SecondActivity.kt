8fd28ef6b7b1a15fbff894ef27d297a307600c91
rkrawec <rkrawec@umich.edu>
2021-10-28 16:15:07 -0400

    Add listview accept decline page

--- UmichConnect/app/src/main/java/com/slackers/umichconnect/SecondActivity.kt
+++ UmichConnect/app/src/main/java/com/slackers/umichconnect/SecondActivity.kt
@@ -2,10 +2,21 @@ package com.slackers.umichconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity: AppCompatActivity() {
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val adapter = ArrayAdapter(this,
+            R.layout.activity_accept_decline_listview_item, array)
+
+        val listView: ListView = findViewById(R.id.listview_2)
+        listView.setAdapter(adapter)
+
     }
 }
