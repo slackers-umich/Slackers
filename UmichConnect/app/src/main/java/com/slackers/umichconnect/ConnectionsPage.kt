be1d8cc43c0fd7ed37e99951a64071509edb218b
pshanka <pshanka@umich.edu>
2021-10-28 15:56:30 -0400

    listview

--- UmichConnect/app/src/main/java/com/slackers/umichconnect/ConnectionsPage.kt
+++ UmichConnect/app/src/main/java/com/slackers/umichconnect/ConnectionsPage.kt
@@ -2,10 +2,21 @@ package com.slackers.umichconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class ConnectionsPage : AppCompatActivity() {
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connections_page)
+
+        val adapter = ArrayAdapter(this,
+            R.layout.activity_connections_page_listview_item, array)
+
+        val listView: ListView = findViewById(R.id.listview_1)
+        listView.setAdapter(adapter)
+
     }
 }
