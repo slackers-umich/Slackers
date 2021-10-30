fb51297c842864eb65cb3be15c4861ddf78651ab
pshanka <pshanka@umich.edu>
2021-10-30 10:17:50 -0400

    Firebase sign in

---
+++ Slackers/UmichConnect/app/src/main/java/com/slackers/umichconnect/ConnectionsPage.kt
@@ -0,0 +1,33 @@
+package com.slackers.umichconnect
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textview.MaterialTextView
+
+class ConnectionsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_connections_page)
+
+            supportActionBar!!.title = "Chat With Your Connections"
+
+        val values = arrayOf("Item Ome", "Item Two", "Item Three", "Item Four", "Item Five")
+            val mListView = findViewById<ListView>(R.id.listView)
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
+            mListView.adapter = adapter
+            mListView.setOnItemClickListener{parent, view, position, id ->
+                startActivity(Intent(this, MainActivity::class.java))
+//                if (position == 0) {
+//                    Toast.makeText(this@ConnectionsPage, "Item One", Toast.LENGTH_SHORT).show()
+//                }
+            }
+
+    }
+}
---
+++ Slackers/UmichConnect/app/src/main/java/com/slackers/umichconnect/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.slackers.umichconnect
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonConnectionsPage = findViewById<Button>(R.id.buttonConnectionsPage)
+        buttonConnectionsPage.setOnClickListener {
+            val intent = Intent(this, ConnectionsPage::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.buttonSignIn).setOnClickListener({startActivity(Intent(this, SignInActivity::class.java))})
+
+    }
+}
