1d41ceb486c9f51f4be093b08343031ce2ac7f66
rkrawec <rkrawec@umich.edu>
2021-10-31 16:06:10 -0400

    Accept Decline button functionality

--- UmichConnect/app/src/main/java/com/slackers/umichconnect/SecondActivity.kt
+++ UmichConnect/app/src/main/java/com/slackers/umichconnect/SecondActivity.kt
@@ -6,10 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import java.text.FieldPosition
 
 class SecondActivity: AppCompatActivity() {
@@ -31,12 +28,16 @@ class SecondActivity: AppCompatActivity() {
     private class MyCustomAdapter(context: Context): BaseAdapter(){
         private val mContext: Context
 
+        private val names = arrayListOf<String>(
+            "Beth", "Ryan", "Mike", "Sanjit", "Carlos", "Mary", "Jaden", "Sora", "Doug", "Jack", "Maya", "Sam", "Henry", "George"
+        )
+
         init{
             mContext = context
         }
 
         override fun getCount(): Int{
-            return 5
+            return names.size
         }
 
         override fun getItemId(position: Int): Long{
@@ -50,10 +51,26 @@ class SecondActivity: AppCompatActivity() {
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
             val layoutInflater = LayoutInflater.from(mContext)
             val rowMain = layoutInflater.inflate(R.layout.row_accept_decline, viewGroup, false)
+
+            val nametextView = rowMain.findViewById<TextView>(R.id.textView)
+            nametextView.text = names.get(position)
+
+
+            val deleteBtn = rowMain.findViewById(R.id.decline) as Button
+            val acceptBtn = rowMain.findViewById(R.id.accept) as Button
+
+            deleteBtn.setOnClickListener {
+                names.removeAt(position)
+                notifyDataSetChanged()
+            }
+
+            acceptBtn.setOnClickListener {
+                names.removeAt(position)
+                notifyDataSetChanged()
+            }
+
+
             return rowMain
-//            val textView = TextView(mContext)
-//            textView.text = "HERE is my row"
-//            return textView
         }
     }
 }
