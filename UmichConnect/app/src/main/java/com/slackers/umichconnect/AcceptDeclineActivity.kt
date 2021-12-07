144d2ba77c3503b97394ca29371004b5d571dfc0
davidwegs <dwegs@umich.edu>
2021-12-07 03:06:32 -0500

    got accept decline working

--- UmichConnect/app/src/main/java/com/slackers/umichconnect/AcceptDeclineActivity.kt
+++ UmichConnect/app/src/main/java/com/slackers/umichconnect/AcceptDeclineActivity.kt
@@ -69,11 +69,9 @@ class AcceptDeclineActivity: AppCompatActivity() {
     private class MyCustomAdapter(context: Context): BaseAdapter(){
         private val mContext: Context
 
-        private val names = arrayListOf<String>(
-            "TEST"
-        )
+        private val names = arrayListOf<String>()
 
-        private val connections = arrayListOf<String>()
+        private val pending_pairs = arrayListOf<Map<String, String>>()
 
         private var firstTime = true
 
@@ -94,55 +92,14 @@ class AcceptDeclineActivity: AppCompatActivity() {
                         .addOnSuccessListener {
                             Log.i("firebase", "Got value ${it.value}")
                             names.add(it.value.toString())
+                            val temp_pair = mapOf(child.getKey().toString() to child.value.toString())
+                            pending_pairs.add(temp_pair)
+                            notifyDataSetChanged()
                         }
                     //val user = h.getValue(String::class.java)
                     //names.add(user.toString())
                 }
             }
-
-
-
-//            var mAuth = FirebaseAuth.getInstance()
-//            var mDbRef = FirebaseDatabase.getInstance().getReference()
-//
-//            var userList = ArrayList<User>()
-//            var adapter = UserAdapter(context, userList)
-//            mDbRef.child("user").addValueEventListener(object: ValueEventListener {
-//                override fun onDataChange(snapshot: DataSnapshot) {
-//                    if (firstTime) {
-//                        userList.clear()
-//                        //userList.add(User("good", "good", "good", listOf<String>("x1","y1")))
-//                        var thisUserPendingNames = listOf<String?>()
-//                        var thisConnections = listOf<String?>()
-//
-//                        for (postSnapshot in snapshot.children) {
-//                            postSnapshot.child("pending")
-//                            val currentUser = postSnapshot.getValue(User::class.java)
-//                            if (mAuth.currentUser?.uid == currentUser?.uid) { // User should not be displayed in list of chats
-//                                currentUser!!.child("pending")
-//                                thisUserPendingNames = currentUser!!.pendingName
-//                                //thisConnections = currentUser!!.connections
-//                            }
-//
-//                        }
-//                        for (postSnapshot in snapshot.children) {
-//                            val currentUser = postSnapshot.getValue(User::class.java)
-//                            if (currentUser?.name in thisUserPendingNames) {
-//                                userList.add(currentUser!!)
-//                                names.add(currentUser!!.name.toString())
-//                            }
-//                            if (currentUser?.uid in thisConnections) {
-//                                connections.add(currentUser!!.uid.toString())
-//                            }
-//                        }
-//                        adapter.notifyDataSetChanged()
-//                        firstTime = false
-//                    }}
-//
-//                override fun onCancelled(error: DatabaseError) {
-//                }
-//
-//            })
         }
 
         override fun getCount(): Int{
@@ -169,103 +126,47 @@ class AcceptDeclineActivity: AppCompatActivity() {
             val acceptBtn = rowMain.findViewById(R.id.accept) as Button
 
             deleteBtn.setOnClickListener {
-
                 var uid: String? = null
-                var database = Firebase.database.reference
+                val database = Firebase.database.reference
                 val user = Firebase.auth.currentUser
                 user?.let {
                     uid = user.uid
                 }
-
-
                 names.removeAt(position)
-                database.child("users/" + uid + "/pending").setValue(names)
-
-
+                pending_pairs.removeAt(position)
+                database.child("users/${uid}/pending").setValue(pending_pairs)
+                    .addOnSuccessListener {
+                        Log.d("firebase", "Deleted request")
+                }
                 notifyDataSetChanged()
-//
-//
-//
-//
-//
-//
-//
-//                var mAuth = FirebaseAuth.getInstance()
-//                var mDbRef = FirebaseDatabase.getInstance().getReference()
-//                var userList = ArrayList<User>()
-//                var adapter = UserAdapter(mContext, userList)
-//
-//                val temp = mapOf<String,List<String>>(
-//                    Pair("pendingName", names)
-//                )
-//                FirebaseDatabase.getInstance().getReference("user").child(mAuth.currentUser?.uid.toString()).updateChildren(temp)
             }
 
             acceptBtn.setOnClickListener {
-
                 var uid: String? = null
-                var database = Firebase.database.reference
+                val database = Firebase.database.reference
                 val user = Firebase.auth.currentUser
                 user?.let {
                     uid = user.uid
                 }
-
-
+                val connect_id = pending_pairs[position].keys.first()
                 names.removeAt(position)
-                database.child("users/" + uid + "/pending").setValue(names)
+                pending_pairs.removeAt(position)
+                database.child("users/${uid}/pending").setValue(pending_pairs)
+                    .addOnSuccessListener {
+                        Log.d("firebase", "Deleted request")
+                    }
 
+                database.child("users/${uid}/connections").push().setValue(connect_id)
+                    .addOnSuccessListener {
+                        Log.d("firebase", "Pushed connection to database")
+                    }
+
+                database.child("users/${connect_id}/connections").push().setValue(uid)
+                    .addOnSuccessListener {
+                        Log.d("firebase", "Pushed connection to database")
+                    }
 
                 notifyDataSetChanged()
-
-
-//                var connections : MutableList<String> = ArrayList()
-//
-//                var uid: String? = null
-//                var database = Firebase.database.reference
-//                val user = Firebase.auth.currentUser
-//                user?.let {
-//                    uid = user.uid
-//                }
-//
-//                database.child("users/"+uid+"/connections").get().addOnSuccessListener {
-//                    for (h in it.children){
-//                        val user = h.getValue(String::class.java)
-//                        connections.add(user.toString())
-//                    }
-//                    connections.add(names[position])
-//                }
-//                database.child("users/" + uid + "/connections").setValue(connections)
-//
-//                names.removeAt(position)
-//                database.child("users/" + uid + "/pending").setValue(names)
-//
-//
-//                notifyDataSetChanged()
-
-
-//                var userWhoWasAccepted = names[position]
-//
-//                names.removeAt(position)
-//                connections.add(userWhoWasAccepted)
-//                notifyDataSetChanged()
-//
-//                var mAuth = FirebaseAuth.getInstance()
-//                var mDbRef = FirebaseDatabase.getInstance().getReference()
-//                var userList = ArrayList<User>()
-//                var adapter = UserAdapter(mContext, userList)
-//
-//                val temp = mapOf<String,List<String>>(
-//                    Pair("pendingName", names)
-//                )
-//                FirebaseDatabase.getInstance().getReference("user").child(mAuth.currentUser?.uid.toString()).updateChildren(temp)
-////                 FirebaseDatabase.getInstance().getReference("user").va//child(mAuth.currentUser?.uid.toString()).child("connections").get().value
-//
-//                val tempConnections = mapOf<String, List<String>>(
-//                    Pair("connections", connections)
-//                )
-//
-//                FirebaseDatabase.getInstance().getReference("user").child(mAuth.currentUser?.uid.toString()).updateChildren(tempConnections)
-
             }
 
 
