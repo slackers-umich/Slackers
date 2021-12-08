f20faed54f02270a57fe7d6d78fa725711a5b130
Andrew Shen <55771349+azshen-umich@users.noreply.github.com>
2021-12-08 12:44:18 -0500

    notifs working as intended

--- UmichConnect/app/src/main/java/com/slackers/umichconnect/MessagingService.kt
+++ UmichConnect/app/src/main/java/com/slackers/umichconnect/MessagingService.kt
@@ -23,20 +23,20 @@ class MessagingService : FirebaseMessagingService() {
     private val channelId = "i.apps.notifications"
     private val description = "Test notification"
 
-    /*override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d(ContentValues.TAG, "Message data payload: ${remoteMessage.data}")
-            val intent = Intent(applicationContext, NearbyActivity::class.java)
-            createNotification(remoteMessage.notification?.body.toString(), intent)
-        }
-    }*/
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.e("urmom2", "test")
+        val intent = Intent(applicationContext, NearbyActivity::class.java)
+        Log.e("urmom2", remoteMessage.notification?.body.toString())
+        notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotification(remoteMessage.notification?.body.toString(), intent)
+    }
 
     private fun createNotification(contentText: String, intentNotif: Intent) {
         val intent2 = intentNotif
         val pendingIntent =
             PendingIntent.getActivity(this, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
             notificationChannel =
                 NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableLights(true)
@@ -46,12 +46,6 @@ class MessagingService : FirebaseMessagingService() {
 
             builder = Notification.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setLargeIcon(
-                    BitmapFactory.decodeResource(
-                        this.resources,
-                        R.drawable.ic_launcher_background
-                    )
-                )
                 .setContentIntent(pendingIntent)
                 .setContentTitle("Umich Connect")
                 .setContentText(contentText)
